ee7f6c508453a3e7651103d06bc83b077f2b0803
Gregor MacLennan <gmaclennan@digital-democracy.org>
2018-10-18 09:23:45 -0700

    Add Redux devtools extension

--- main/app.js
+++ main/app.js
@@ -5,7 +5,7 @@ import path from 'path'
 import {app, Menu, BrowserWindow, ipcMain, dialog} from 'electron'
 import Config from 'electron-store'
 import mkdirp from 'mkdirp'
-import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer'
+import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from 'electron-devtools-installer'
 import getPorts from 'get-ports'
 import url from 'url'
 import pump from 'pump'
@@ -85,7 +85,7 @@ function onAppReady () {
   mainWindow = setupWindow()
 
   if (isDev) {
-    installExtension(REACT_DEVELOPER_TOOLS)
+    installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS])
       .then((name) => console.log(`Added Extension:  ${name}`))
       .catch((err) => console.log('An error occurred: ', err))
     // installExtension(REDUX_DEVTOOLS)
