5103ec85072c14b038388ec1748d147ebba2f34c
Gregor MacLennan <gmaclennan@digital-democracy.org>
2018-11-26 16:12:49 +0000

    Fix mediaserver

--- main/media_server.js
+++ main/media_server.js
@@ -1,6 +1,6 @@
 var mime = require('mime')
 
-function serveFile (req, res, filepath, media, next) {
+function serveFile (req, res, filepath, media) {
   var r = media.createReadStream(filepath)
   r.once('error', function (err) {
     console.error('Error serving ' + filepath + ':', err)
@@ -14,11 +14,11 @@ function serveFile (req, res, filepath, media, next) {
 module.exports = function (media, root) {
   root = root.replace(/\/$/, '') || ''
   const rootRegExp = new RegExp('^' + root + '/')
-  var handler = function (req, res, next) {
-    if (!req.url.startsWith(root)) return next()
-    if (req.method !== 'GET') return next()
+  var handler = function (req, res) {
+    if (!req.url.startsWith(root)) return
+    if (req.method !== 'GET') return
     var filepath = req.url.replace(rootRegExp, '')
-    serveFile(req, res, filepath, media, next)
+    serveFile(req, res, filepath, media)
   }
   return handler
 }
