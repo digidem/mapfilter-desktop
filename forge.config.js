7e4a1d4d72c295f42b599ed16ce9eacf0776e3a3
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-01-10 18:20:56 +0000

    Fix: reduce bundle size by ignoring some modules

--- forge.config.js
+++ forge.config.js
@@ -6,6 +6,14 @@ var useAsar = isWin || isMac
 
 var isPrerelease = !!semver.prerelease(process.env.TRAVIS_TAG)
 
+var ignore = [
+  '/node_modules(?:/|.*)(?:test|__tests__|tests|powered-test|example|examples)(?:$|/)',
+  '/node_modules/.*\\.d\\ts$',
+  '/node_modules(?:/|.*)(?:CHANGELOG.md|README.md|README|readme.md|readme)(?:$|/)',
+  '.*/(:?appveyor\\.yml|travis\\.yml|circle\\.yml)$',
+  '.*/(:?__pycache__|thumbs\\.db|\\.flowconfig|\\.idea,\\.vs|\\.nyc_output)(?:$|/)'
+]
+
 module.exports = {
   "make_targets": {
     "win32": [
@@ -26,6 +34,7 @@ module.exports = {
     "executableName": "tizii-tizii",
     "extraResource": "resources/launcher.sh",
     "icon": "static/mapfilter",
+    "ignore": ignore,
     "win32metadata": {
       "CompanyName": "Digital Democracy"
     }
